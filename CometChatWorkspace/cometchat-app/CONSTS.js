ca69bcab0197839636d944767749c9f72399474c
beulah <beulah@missmalini.com>
2021-12-07 15:29:20 +0530

    App credentials

--- CometChatWorkspace/cometchat-app/CONSTS.js
+++ CometChatWorkspace/cometchat-app/CONSTS.js
@@ -1,5 +1,5 @@
 export const COMETCHAT_CONSTANTS = {
-  APP_ID: 'APP_ID',
-  REGION: 'REGION',
-  AUTH_KEY: 'AUTH_KEY',
+  APP_ID: '198891bd6b4a1a4c',
+  REGION: 'us',
+  AUTH_KEY: 'b86d8ddbf3e87b224a294af8abc37b81f58b16cb',
 };
